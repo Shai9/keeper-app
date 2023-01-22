5b9deaae8093ac10e27d6cc03fe54db2fccbcf3f
shai9 <dennis.shakava@student.moringaschool.com>
2023-01-22 23:07:22 +0300

    finish on footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,13 @@ function Footer() {
 
   const currentYear = new Date().getFullYear();
   return (
-      <p className='relative bottom-0 '>
+      <p className='bg-gray-200
+      text-2xl text-black text-center
+      border-t-4
+      fixed
+      inset-x-0
+      bottom-0
+      p-4'>
         Copyright © {currentYear}
       </p>
   )
